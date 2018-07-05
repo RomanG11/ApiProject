2ac01c17a2e00d32a9cf30e14fb253ff8c26a2a2
Roman <33402067+RomanG11@users.noreply.github.com>
2018-07-05 14:21:34 +0300

    Add files via upload

--- contract/truffle.js
+++ contract/truffle.js
@@ -1,8 +1,10 @@
+const config = require('./config.json');
+
 const PrivateKeyProvider = require("truffle-privatekey-provider");
 
-const privateKey = "8c9da8046bb0d7ae1687dac46b3515224d7c0c398c7a6769167cc9a331982430";
+// const privateKey = "8c9da8046bb0d7ae1687dac46b3515224d7c0c398c7a6769167cc9a331982430";
 
-const provider =  new PrivateKeyProvider(privateKey, "https://rinkeby.infura.io/jV2i3C9g4hfww8EoSpHs");
+const provider =  new PrivateKeyProvider(config.privateKey, config.host);
 
 module.exports = {
   networks: {
@@ -11,14 +13,11 @@ module.exports = {
       port: 8545,
       network_id: "*"
     },
-    	rinkeby: {
-      // provider: function() {
-        // return new HDWalletProvider(privateKey, "https://rinkeby.infura.io/jV2i3C9g4hfww8EoSpHs")
-      // },
+    	production: {
       provider: provider,
-      network_id: 4,
+      network_id: 1,
       gas: 4000000,
-      gasPrice: 21000000000
+      gasPrice: config.gasPrice
     }
   }
 };
